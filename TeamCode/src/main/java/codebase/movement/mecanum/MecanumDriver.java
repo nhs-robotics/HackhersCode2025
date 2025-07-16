c7b3f288ca24049ef64341e8cbd8d40ae5e5c074
Consolegamer2 <160079707+Consolegamer2@users.noreply.github.com>
2025-07-16 13:37:39 -0400

    add MecanumCoefficientMatrix, MecanumCoefficientSet, MecanumDriver, and MovementVector

--- TeamCode/src/main/java/codebase/movement/mecanum/MecanumDriver.java
+++ TeamCode/src/main/java/codebase/movement/mecanum/MecanumDriver.java
@@ -8,23 +8,40 @@ public class MecanumDriver {
     public final Motor fr;
     public final Motor bl;
     public final Motor br;
+    public final MecanumCoefficientMatrix omniDriveCoefficients;
 
     public MecanumDriver(
             Motor fl,
             Motor fr,
             Motor bl,
-            Motor br
+            Motor br,
+            MecanumCoefficientMatrix omniDriveCoefficients
     ) {
         this.fl = fl;
         this.fr = fr;
         this.bl = bl;
         this.br = br;
+        this.omniDriveCoefficients = omniDriveCoefficients;
     }
 
-
-    public void setVelocity(MovementVector velocity){
-        MecanumCoefficientSet
+    public void setMotorVelocities(double fl, double fr, double bl, double br) {
+        this.fl.setVelocity(fl);
+        this.fr.setVelocity(fr);
+        this.bl.setVelocity(bl);
+        this.br.setVelocity(br);
     }
 
+    public void setRelativeVelocity(MovementVector velocity) {
+        MecanumCoefficientSet coefficientSet = this.omniDriveCoefficients.calculateCoefficientsWithPower(
+                velocity.getVertical(),
+                velocity.getHorizontal(),
+                velocity.getRotation()
+        );
+
+        this.setMotorVelocities(coefficientSet.fl, coefficientSet.fr, coefficientSet.bl, coefficientSet.br);
+
+    }
+
+//.    public void setAbsoluteVelocity()
 
 }
