10a2025c8b0b050e7bacfa8a3b3a69212d015691
liambridgers <ltb271@students.needham.k12.ma.us>
2025-08-06 15:45:17 -0400

    change targetPosition to be a supplier, add an errorSupplier to allow for irregular error calculations (e.g. angles)

--- TeamCode/src/main/java/codebase/controllers/PIDController.java
+++ TeamCode/src/main/java/codebase/controllers/PIDController.java
@@ -10,24 +10,29 @@ public class PIDController {
 
     private final Supplier<Double> currentPositionSupplier;
 
-    private double targetPosition;
+    private final Supplier<Double> targetPositionSupplier;
+
+    private Supplier<Double> errorSupplier;
 
     private double integralSum = 0;
     private double lastError = 0;
     private double lastTime = 0;
 
-
-    public PIDController(double Kp, double Ki, double Kd, Supplier<Double> currentPositionSupplier, double targetPosition) {
+    public PIDController(double Kp, double Ki, double Kd, Supplier<Double> currentPositionSupplier, Supplier<Double> targetPositionSupplier) {
         this.Kp = Kp;
         this.Ki = Ki;
         this.Kd = Kd;
 
         this.currentPositionSupplier = currentPositionSupplier;
-        this.targetPosition = targetPosition;
+        this.targetPositionSupplier = targetPositionSupplier;
+
+        this.errorSupplier = () -> this.targetPositionSupplier.get() - this.currentPositionSupplier.get();
     }
 
-    public void setTargetPosition(double targetPosition) {
-        this.targetPosition = targetPosition;
+    public PIDController(double Kp, double Ki, double Kd, Supplier<Double> currentPositionSupplier, Supplier<Double> targetPositionSupplier, Supplier<Double> errorSupplier) {
+        this(Kp, Ki, Kd, currentPositionSupplier, targetPositionSupplier);
+
+        this.errorSupplier = errorSupplier;
     }
 
     public double getPower() {
@@ -39,9 +44,7 @@ public class PIDController {
         double deltaTime = System.currentTimeMillis() - lastTime;
         double deltaTimeSeconds = deltaTime / 1000;
 
-        double currentPosition = currentPositionSupplier.get();
-
-        double error = targetPosition - currentPosition;
+        double error = errorSupplier.get();
 
         double derivative = (error - lastError) / deltaTimeSeconds;
 
